773a815650c60006311e77c25a6ef8e5aef0ca0e
ludmattern <github.rfgrg@passmail.net>
2025-05-12 11:05:24 +0200

    cleaned obj and bin

--- DrQUine/C/Grace_kid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-/*
-	Graceful self-replication
-*/
-#define FILENAME "Grace_kid.c"
-#define CODE "#include <stdio.h>%c/*%c%cGraceful self-replication%c*/%c#define FILENAME %cGrace_kid.c%c%c#define CODE %c%s%c%c#define FT() int main(){FILE *f=fopen(FILENAME,%cw%c);if(f){fprintf(f,CODE,10,10,9,10,10,34,34,10,34,CODE,34,10,34,34,10);fclose(f);}return 0;}%cFT()"
-#define FT() int main(){FILE *f=fopen(FILENAME,"w");if(f){fprintf(f,CODE,10,10,9,10,10,34,34,10,34,CODE,34,10,34,34,10);fclose(f);}return 0;}
-FT()
